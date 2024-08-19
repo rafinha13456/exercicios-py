1f1fc6a3b95eb340c9e28de366eff262269afd76
rafael <ric456eli@gmail.com>
2024-08-19 20:30:33 -0300

    Create exercicio03.py
    adicionado exercicio03

---
+++ exercicio03.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite o 1º numero: "))
+n2 = int(input("Digite o 2º numero: "))
+n3 = int(input("Digite o 3º numero: "))
+n4 = int(input("Digite o 4º numero: "))
+resultado = n1+n2+n3
+print(f"A soma dos numeros é: {resultado}")
