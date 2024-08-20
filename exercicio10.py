6b0d6c592624e26d706229e135375552191d57d2
rafael <ric456eli@gmail.com>
2024-08-20 20:25:29 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,15 +1,16 @@
-def tipo_triangulo(a, b, c):
-    if a == b and b == c:
-        return "Equilátero"
-    elif a == b or a == c or b == c:
-        return "Isósceles"
-    else:
-        return "Escaleno"
-
+# Recebe os três lados do triângulo
 lado1 = float(input("Digite o primeiro lado do triângulo: "))
 lado2 = float(input("Digite o segundo lado do triângulo: "))
 lado3 = float(input("Digite o terceiro lado do triângulo: "))
 
-tipo = tipo_triangulo(lado1, lado2, lado3)
-
-print("O triângulo é", tipo)
+# Verifica se os lados formam um triângulo
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    # Verifica o tipo de triângulo
+    if lado1 == lado2 == lado3:
+        print("O triângulo é equilátero.")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print("O triângulo é isósceles.")
+    else:
+        print("O triângulo é escaleno.")
+else:
+    print("Os lados informados não formam um triângulo.")2
